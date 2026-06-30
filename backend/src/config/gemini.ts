6e59fdab1de5d91f2e57a2743acb72a113ccd331
DIMAX99 <divitc2004@gmail.com>
2026-06-30 23:41:58 +0530

    Added LangSmith for gemini tracing Added .env.example files for backend and frontend

--- backend/src/config/gemini.ts
+++ backend/src/config/gemini.ts
@@ -1,12 +1,17 @@
 import { GoogleGenAI } from "@google/genai";
+import * as wrappers from 'langsmith/wrappers';
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY!,
 });
 
+
+const client = wrappers.wrapSDK(ai);
+
+
 export class AIService {
   async generateResponse(prompt: string) {
-    const response = await ai.models.generateContent({
+    const response = await client.models.generateContent({
       model: "gemini-2.5-flash",
       contents: prompt,
     });
